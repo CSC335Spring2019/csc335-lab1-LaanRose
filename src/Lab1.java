f22cc8883723990414aaea0666614740445f5acf
Laan V Rose <laanrose@gmail.com>
2019-01-16 10:21:34 -0700

    Change message to have my name
    Change println() to have my name in it.

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,14 +1,14 @@
 /**
  * 
- * @author YOUR NAME HERE
- * 
+ * @author Laan Rose
+ * CSC 335, Lab Section C (10:00-11:00AM)
  * 
  */
 public class Lab1 {
 
 	public static void main(String[] args) {
 		// TODO Change this to "Hello CSC 335, I'm YOURNAME"
-		System.out.println("Hello, World!");
+		System.out.println("Hello CSC 335, I'm Laan Rose");
 	}
 
 }
